0e98829ade8f45d450d87c741162f2efc472f37d
Cambe <zakharkin@cdo-global.ru>
2025-06-09 10:03:49 +0300

    Изменен файл конфигурации Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
-    supportFile: 'cypress/support/e2e.ts',
+    supportFile: false,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     video: false,
     screenshotOnRunFailure: true,
